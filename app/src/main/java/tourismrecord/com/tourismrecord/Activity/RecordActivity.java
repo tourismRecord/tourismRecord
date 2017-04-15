28e6e32e57b0fbb5061306283675a047fa83d0b5
xingyingyue <945603130@qq.com>
2017-04-16 01:23:02 +0800

    add second sharecontentlist

--- app/src/main/java/tourismrecord/com/tourismrecord/Activity/RecordActivity.java
+++ app/src/main/java/tourismrecord/com/tourismrecord/Activity/RecordActivity.java
@@ -6,11 +6,18 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tourismrecord.com.tourismrecord.Adapter.ShareContentAdapter;
+import tourismrecord.com.tourismrecord.Model.ShareContent;
 import tourismrecord.com.tourismrecord.R;
 
 /**
@@ -21,24 +28,34 @@ public class RecordActivity extends BaseActivity implements NavigationView.OnNav
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private List<ShareContent> shareContentList=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initList();
         initView();
     }
 
     void initView(){
         toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         drawerLayout=(DrawerLayout)findViewById(R.id.main_drawerLayout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.setDrawerListener(toggle);
         toggle.syncState();
+
         navigationView=(NavigationView)findViewById(R.id.nav_view);
         navigationView.setCheckedItem(R.id.personalInfo);
         navigationView.setNavigationItemSelectedListener(this);
+
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.share_content_list);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        ShareContentAdapter adapter=new ShareContentAdapter(shareContentList);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -90,5 +107,9 @@ public class RecordActivity extends BaseActivity implements NavigationView.OnNav
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private void initList(){
+        ShareContent shareContent=new ShareContent("xingyingyue",R.mipmap.ic_launcher,"no_title","2017-04-16");
+        shareContentList.add(shareContent);
     }
 }
